a08ec2fa93f0f7d7c686a1d71ef3bfab8dd9be7c
Zete Lui <mas@k.ed>
2013-06-07 22:22:17 +0800

    send random iv along with the session message

--- lib/nyara/session.rb
+++ lib/nyara/session.rb
@@ -4,12 +4,13 @@ module Nyara
   module Session
     extend self
 
+    CIPHER_BLOCK_SIZE = 256/8
+
     # session is by default DSA + SHA384/SHA1 signed, sub config options are:
     #
     # - name       (session entry name in cookie, default is 'spare_me_plz')
     # - key        (DSA private key string, in der or pem format, use random if not given)
-    # - cipher_key (if exist, use aes-256-cbc to cipher the "sig&json")
-    # - cipher_iv  (aes iv, use key.reverse if not given)
+    # - cipher_key (if exist, use aes-256-cbc to cipher the "sig&json", the first 256bit is sliced for iv)
 
     # init from config
     def init
@@ -25,15 +26,7 @@ module Nyara
       # DSA can sign on any digest since 1.0.0
       @dss = OpenSSL::VERSION >= '1.0.0' ? OpenSSL::Digest::SHA384 : OpenSSL::Digest::DSS1
 
-      if @cipher_key = pad_256_bit(c['cipher_key'])
-        if c['cipher_iv'].to_s.empty?
-          @cipher_iv = @cipher_key.reverse
-        else
-          @cipher_iv = pad_256_bit c['cipher_iv']
-        end
-      else
-        @cipher_iv = nil
-      end
+      @cipher_key = pad_256_bit c['cipher_key']
     end
 
     attr_reader :name
@@ -73,13 +66,20 @@ module Nyara
     # private
 
     def cipher str
-      c = new_cipher true
-      Base64.urlsafe_encode64(c.update(str) << c.final)
+      iv = rand(36**CIPHER_BLOCK_SIZE).to_s(36)
+      c = new_cipher true, iv
+      Base64.urlsafe_encode64(iv.dup << c.update(str) << c.final)
     end
 
     def decipher str
       str = Base64.urlsafe_decode64 str
-      c = new_cipher false
+      return '' if str.bytesize % CIPHER_BLOCK_SIZE != 0
+
+      iv = str.byteslice 0...CIPHER_BLOCK_SIZE
+      return '' if iv.bytesize < CIPHER_BLOCK_SIZE
+
+      str = str.byteslice CIPHER_BLOCK_SIZE..-1
+      c = new_cipher false, iv
       begin
         c.update(str) << c.final
       rescue OpenSSL::Cipher::CipherError
@@ -90,7 +90,7 @@ module Nyara
     def pad_256_bit s
       s = s.to_s
       return nil if s.empty?
-      len = 256/8
+      len = CIPHER_BLOCK_SIZE
       s[0...len].ljust len, '*'
     end
 
@@ -99,11 +99,11 @@ module Nyara
       ParamHash.new
     end
 
-    def new_cipher encrypt
+    def new_cipher encrypt, iv
       c = OpenSSL::Cipher.new 'aes-256-cbc'
       encrypt ? c.encrypt : c.decrypt
       c.key = @cipher_key
-      c.iv = @cipher_iv
+      c.iv = iv
       c
     end
   end
--- spec/session_spec.rb
+++ spec/session_spec.rb
@@ -39,17 +39,20 @@ module Nyara
 
       it "encode and decode" do
         cookie = {}
-        session = {'hello' => 'world2'}
-        Session.encode session, cookie
-
-        # should be 256bit aligned even src length changed
-        assert_equal 0, cookie[Session.name].bytesize % (256/8)
         session = {'hello' => 'world'}
         Session.encode session, cookie
-        assert_equal 0, cookie[Session.name].bytesize % (256/8)
+
+        assert_not_includes cookie[Session.name], 'world'
 
         session2 = Session.decode cookie
         assert_equal 'world', session2[:hello]
+      end
+
+      it "cipher should not be pure" do
+        message = 'OCB is by far the best mode, as it allows encryption and authentication in a single pass. However there are patents on it in USA.'
+        r1 = Session.cipher message
+        r2 = Session.cipher message
+        assert_not_equal r1, r2
       end
     end
   end
