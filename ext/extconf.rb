8cfced7b3a6e55d7c6bb45a19dd7b72ab3d74cb3
Zete Lui <mas@k.ed>
2013-07-12 17:44:52 +0800

    add use_gcc42 for debugging low versions of gcc

--- ext/extconf.rb
+++ ext/extconf.rb
@@ -19,6 +19,13 @@ def tweak_cflags
   puts "To enable debug: make xflags='-DDEBUG -O0'"
 end
 
+# for debugging low versions of gcc
+def use_gcc42
+  RbConfig::MAKEFILE_CONFIG['CC'] = `which gcc-4.2`.strip
+  RbConfig::MAKEFILE_CONFIG['CXX'] = `which g++-4.2`.strip
+end
+# use_gcc42
+
 have_kqueue = (have_header("sys/event.h") and have_header("sys/queue.h"))
 have_epoll = have_func('epoll_create', 'sys/epoll.h')
 abort('no kqueue nor epoll') if !have_kqueue and !have_epoll
