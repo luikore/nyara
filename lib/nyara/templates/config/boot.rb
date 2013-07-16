67c026a0c83c2962dc18556943c82a2f5c411c69
Jason Lee <huacnlee@gmail.com>
2013-07-16 12:58:44 +0800

    boot.rb with run, configs move into application.rb

--- lib/nyara/templates/config/boot.rb
+++ lib/nyara/templates/config/boot.rb
@@ -1,24 +1,4 @@
-require "nyara"
-require "erubis"
-require 'bundler' and Bundler.setup(:default)
-require 'mongoid'
+require_relative "application"
 
-configure do
-  set :views, 'app/views'
-  set :session, :name, '_aaa'
-  set :session, :secure, true
-  set :session, :expires, 30 * 60
-  
-  map '/', 'welcome'
-end
-
-# Configure Mongoid
-Mongoid.load!(File.join(Nyara.config.root,'config/database.yml'), Nyara.config.env)
-
-# TODO: 此处不用会报 Nyara::SimpleController: no action defined (RuntimeError)
-get '/' do
-  send_string ''
-end
-
-# require_relative NYARA_ENV
-
+Nyara.setup
+Nyara.start_server
