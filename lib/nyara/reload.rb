eeda07a4dab296e8eaf1a688ed349aaee7c89e44
Zete Lui <mas@k.ed>
2013-07-17 13:06:23 +0800

    refine reloader implementation

--- lib/nyara/reload.rb
+++ lib/nyara/reload.rb
@@ -4,52 +4,77 @@ module Nyara
   # listen to fs events and reload code / views
   # todo add to development env: require 'nyara/reload'; Reload.listen
   module Reload
-    def self.listen
-      Listen.to Config.views_path('/'), relative_paths: true do |modified, added, removed|
-        modified.each do |file|
-          View.on_modified file
-        end
-        removed.each do |file|
-          View.on_removed file
+    # init, should require all files that needs to be reloaded in the given block
+    def self.init
+      @new_classes = []
+      @trace_point = TracePoint.new :class do |tp|
+        @new_classes << tp.self.to_s.to_sym
+      end
+      @file_list = {}
+      @first_load = true
+      yield
+      @first_load = false
+    end
+
+    # NOTE file should end with '.rb'
+    def self.load_file file
+      if consts = @file_list[file]
+        consts.reverse_each do |const|
+          Object.send :remove_const, const
         end
       end
 
-      return unless Config['reload_paths']
-      Listen.to Config.root, filter: Config['reload_paths'] do |modified, added, removed|
-        removed.each do
-          reload_all
-          return
+      @trace_point.enable
+      old_consts = Object.send :constants
+      if @first_load
+        require file
+      else
+        if l = Nyara.logger
+          l.info "reloading: #{file}"
         end
+        begin
+          load file
+          @last_error = nil
+        rescue Exception
+          @last_error = $!
+        end
+      end
+      @trace_point.disable
+      added_consts = Object.send(:constants) - old_consts
 
-        changed = false
+      added_consts.concat @new_classes
+      @new_classes.clear
+      added_consts.uniq!
+      added_consts.sort!
 
-        added.each do |file|
-          if file.end_with?('.rb')
-            begin
-              require file
-            rescue Exception
-              puts $!
-              puts $!.backtrace
-            end
-            changed = true
+      @file_list[file] = added_consts
+      @last_error
+    end
+
+    # start listening
+    def self.listen
+      views_path = Config.views_path('/')
+      if views_path
+        if l = Nyara.logger
+          l.info "watching views change under #{views_path}"
+        end
+        Listen.to Config.views_path('/'), relative_paths: true do |modified, added, removed|
+          modified.each do |file|
+            View.on_modified file
+          end
+          removed.each do |file|
+            View.on_removed file
           end
         end
+      end
 
-        modified.each do |file|
-          if file.end_with?('.rb')
-            begin
-              reload file
-            rescue Exception
-              puts $!
-              puts $!.backtrace
-            end
-            changed = true
-          end
-        end
-
-        if changed
-          Nyara.setup
-          # todo after-reload hook
+      return unless Config.development?
+      if l = Nyara.logger
+        l.info "watching app change under #{Config['root']}"
+      end
+      Listen.to Config['root'], filter: /\.rb$/, relative_paths: false do |modified, added, removed|
+        (added + modified).uniq.each do |file|
+          load_file file
         end
         # (1.0) todo send notification on bad files
       end
