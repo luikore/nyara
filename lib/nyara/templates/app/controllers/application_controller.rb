3643926a5534d8b88697f80444f854a5a698271b
Saito <saitowu@gmail.com>
2013-08-16 23:45:25 +0800

    nyara marry with linner

--- lib/nyara/templates/app/controllers/application_controller.rb
+++ lib/nyara/templates/app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < Nyara::Controller
   set_default_layout 'layouts/application'
+
+  def asset_path(path)
+    return path if Nyara.config.env == "development"
+    manifest[path]
+  end
+
+private
+  def manifest
+    @manifest ||= begin
+      env = Linner.env
+      manifest_file = File.join(env.public_folder, env.manifest)
+      YAML.load File.read(manifest_file)
+    end
+  end
 end
